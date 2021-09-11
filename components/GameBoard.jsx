5da2dc8bf07d73f6fa0cb02e018db1d7bbb26bc7
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-11 21:03:36 +1200

    set styling to text inside cell

--- components/GameBoard.jsx
+++ components/GameBoard.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GameBoard = () => {
+
+
+    const [move, setMove] = useState(null)
+
+    const possibleMoves = [['X'], ['Y'], ['X'], ['Y'], ['X'], ['Y'], ['X'], ['Y'], ['X']]
+
+    const playerMoves = () => {
+        setMove(possibleMoves[0][0])
+    }
+
+    // Each cell needs to be independent so when state is func can check current state of single cell and updated as needed.
+    
+    // Things to figure out:
+    // How to have each move alternate between 'X' & 'Y' - can't overide each other
+    // Array of 9 X & Y strings - each click cycles to next position in the array
 
     return(
         <div className="gameBoard">
             <div className="gameBoard-container">
-                <div className="gameBoard-cell"></div>
-                <div className="gameBoard-cell"></div>
-                <div className="gameBoard-cell"></div>
-                <div className="gameBoard-cell"></div>
-                <div className="gameBoard-cell"></div>
-                <div className="gameBoard-cell"></div>
-                <div className="gameBoard-cell"></div>
-                <div className="gameBoard-cell"></div>
-                <div className="gameBoard-cell"></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
+                <div className="gameBoard-cell" onClick={playerMoves}><h1 className='cell-text'>{move}</h1></div>
             </div>
         </div>
     )

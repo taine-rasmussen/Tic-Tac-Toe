2527654b1778516b83d11a13fa297ca442cc5875
Taine Rasmussen <taine.rasmussen@gmail.com>
2021-09-14 14:27:58 +1200

    working version of player tracker

--- components/GameBoard.jsx
+++ components/GameBoard.jsx
@@ -6,10 +6,10 @@ const GameBoard = () => {
     const [move, setMove] = useState(null)
 
     // Tracks player moves
-    const [tracker, setTracker] = useState([[''],[''],[''],
-                                            [''],[''],[''],
-                                            [''],[''],['']])
-    // Cell inputs for 'X' & 'Y'
+    // const [tracker, setTracker] = useState([[''],[''],[''],
+    //                                         [''],[''],[''],
+    //                                         [''],[''],['']])
+    // Cell inputs for 'X' & 'O'
     const [input1, setInput1] = useState(null)
     const [input2, setInput2] = useState(null)
     const [input3, setInput3] = useState(null)
@@ -20,7 +20,16 @@ const GameBoard = () => {
     const [input8, setInput8] = useState(null)
     const [input9, setInput9] = useState(null)
 
-
+    // Cell values to tracker state of each cell as players make moves throughout the game
+    const [cellTracker1, setCellTracker1] = useState('empty')
+    const [cellTracker2, setCellTracker2] = useState('empty')
+    const [cellTracker3, setCellTracker3] = useState('empty')
+    const [cellTracker4, setCellTracker4] = useState('empty')
+    const [cellTracker5, setCellTracker5] = useState('empty')
+    const [cellTracker6, setCellTracker6] = useState('empty')
+    const [cellTracker7, setCellTracker7] = useState('empty')
+    const [cellTracker8, setCellTracker8] = useState('empty')
+    const [cellTracker9, setCellTracker9] = useState('empty')
 
 
 
@@ -40,7 +49,7 @@ const GameBoard = () => {
     
     // Possible player moves
     const X = 'X'
-    const Y = 'Y'
+    const O = 'O'
 
         // Handles reset btn
         const Reset = () => {
@@ -58,73 +67,77 @@ const GameBoard = () => {
             setInput9(null)
         }
 
+        let tracker = [cellTracker1, cellTracker2, cellTracker3, cellTracker4, cellTracker5, cellTracker6, cellTracker7, cellTracker8, cellTracker9]
+
+        
+
         // Cell onClick func that handles player selecting a cell
         const cell1 = () => {
             if (move % 2 === 0){
-                setInput1('X'), setMove(move + 1), setTracker(tracker[0] = 'X')
+                setInput1('X'), setMove(move + 1), setCellTracker1('X')
             }   else {
-                setInput1('Y'), setMove(move + 1), setTracker(tracker[0] = 'Y')
+                setInput1('Y'), setMove(move + 1), setCellTracker1('O')
             }
         }
         const cell2 = () => {
             if (move % 2 === 0){
-                setInput2('X'), setMove(move + 1)
+                setInput2('X'), setMove(move + 1), setCellTracker2('X')
             }   else {
-                setInput2('Y'), setMove(move + 1)
+                setInput2('O'), setMove(move + 1), setCellTracker2('O')
             }
         }
         const cell3 = () => {
             if (move % 2 === 0){
-                setInput3('X'), setMove(move + 1)
+                setInput3('X'), setMove(move + 1), setCellTracker3('X')
             }else {
-                setInput3('Y'), setMove(move + 1)
+                setInput3('O'), setMove(move + 1), setCellTracker3('O')
             }
         }
         const cell4 = () => {
             if (move % 2 === 0){
-                setInput4('X'), setMove(move + 1)
+                setInput4('X'), setMove(move + 1), setCellTracker4('X')
             }else {
-                setInput4('Y'), setMove(move + 1)
+                setInput4('O'), setMove(move + 1), setCellTracker4('O')
             }
         }
         const cell5 = () => {
             if (move % 2 === 0){
-                setInput5('X'), setMove(move + 1)
+                setInput5('X'), setMove(move + 1), setCellTracker5('X')
             }else {
-                setInput5('Y'), setMove(move + 1)
+                setInput5('O'), setMove(move + 1), setCellTracker5('O')
             }
         }
         const cell6 = () => {
             if (move % 2 === 0){
-                setInput6('X'), setMove(move + 1)
+                setInput6('X'), setMove(move + 1), setCellTracker6('X')
             }else {
-                setInput6('Y'), setMove(move + 1)
+                setInput6('O'), setMove(move + 1), setCellTracker6('O')
             }
         }
         const cell7 = () => {
             if (move % 2 === 0){
-                setInput7('X'), setMove(move + 1)
+                setInput7('X'), setMove(move + 1), setCellTracker7('X')
             }else {
-                setInput7('Y'), setMove(move + 1)
+                setInput7('O'), setMove(move + 1), setCellTracker7('O')
             }
         }
         const cell8 = () => {
             if (move % 2 === 0){
-                setInput8('X'), setMove(move + 1)
+                setInput8('X'), setMove(move + 1), setCellTracker8('X')
             }else {
-                setInput8('Y'), setMove(move + 1)
+                setInput8('O'), setMove(move + 1), setCellTracker8('O')
             }
         }
         const cell9 = () => {
             if (move % 2 === 0){
-                setInput9('X'), setMove(move + 1)
+                setInput9('X'), setMove(move + 1), setCellTracker9('X')
             }else {
-                setInput9('Y'), setMove(move + 1)
+                setInput9('O'), setMove(move + 1), setCellTracker9('O')
             }
         }
 
     console.log('move:', move)
-    console.log('Player moves:', tracker[0])
+    console.log('Player moves:', tracker)
     // Things to figure out:
     // Each time player moves update move tracker array and declare what was placed on the
     // Will need to write all possible win conditions for the game and check if they are already met when player goes to move again
